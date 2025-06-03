5eac1a716679a6aca63f59506bdb01ceda06d083
Kassia-Hikari <kassiahikari@outlook.com>
2025-06-03 19:00:36 -0400

    Add files via upload

---
+++ Dicionario.py
@@ -0,0 +1,20 @@
+# dados=dict()
+# dados= {'nome':'Laura', 'idade':'44'}
+# dados['sexo']='Feminino'
+# dados['Estado Civil']='Solteira'
+# del dados['Estado Civil']
+# print(dados)
+
+# dados = dict()
+# Alunos = list()
+# for c in range (0,2):
+#     dados['Nome'] = str(input('Digite seu nome: '))
+#     dados['Idade'] = int(input('Digite sua idade: '))
+#     dados['Matricula'] = int(input('Digite sua matricula: '))
+#     Alunos.append(dados.copy())
+# for d in Alunos:
+#     print(d)
+
+
+
+
